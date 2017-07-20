3442ed75afef1781776ca1b1f35ee4d98c31dd70
Henry Mustapha <holyfireangel123@gmail.com>
2017-07-20 19:16:17 +0000

    new stuff

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
     $.get("navbar.html", function(data){
         $(".bar").replaceWith(data);
     });
-    $('ul li').click(function(){
-        var clicked_tab = $(this);
-        clicked_tab.addClass('active');
-        clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
-        var clicked_href = $(this).children('a').attr('href');
-        $('img').hide();
-        $(clicked_href).show();
+   $('ul li').click(function(){
+    var clicked_tab = $(this);
+    clicked_tab.addClass('active');
+    clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
     }); 
     
     $("#myCarousel").carousel("cycle");
     
+    $("#button").hover(function(){
+     
+    });
 });
