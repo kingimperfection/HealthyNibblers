d7df68f6a93d66c2ab64468010b126fad67adbdf
Henry Mustapha <holyfireangel123@gmail.com>
2017-07-21 16:34:06 +0000

    eh

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 /*global $*/
 $(document).ready(function() {
     
-    
+    $.get("head.html", function(data){
+        $("#head").replaceWith(data);
+    });
+    $.get("navbar.html", function(data){
+        $(".bar").replaceWith(data);
+    });
+   $('ul li').click(function(){
+    var clicked_tab = $(this);
+    clicked_tab.addClass('active');
+    clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
+    }); 
     
     $("#myCarousel").carousel("cycle");
     
